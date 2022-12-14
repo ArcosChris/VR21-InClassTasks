d14e8b29cac30ff24b759d94a895bbdb4b86918e
Christopher Arcos <arcoschris456@gmail.com>
2022-12-13 16:00:20 -0800

    added css changes

--- TypeAhead/scripts.js
+++ TypeAhead/scripts.js
@@ -1,4 +1,21 @@
-const arrayOfObjects = [{ name: "Fox", link: "foxnews.com" }, { name: "Fandango", link: "fandango.com" }, { name: "CNN", link: "cnn.com" }];
+const arrayOfObjects = [
+    { name: "Fox", link: "foxnews.com" },
+    { name: "Fandango", link: "fandango.com" },
+    { name: "CNN", link: "cnn.com" },
+
+
+    { name: "ABC", link: "abc.com" },
+    { name: "Disney", link: "disney.com" },
+    { name: "Disney+", link: "disneyplus.com" },
+
+    { name: "CBS", link: "cbs.com" },
+    { name: "Netflix", link: "netflix.com" },
+    { name: "Hulu", link: "hulu.com" },
+
+    { name: "Amazon Prime", link: "amazon.com/amazonprime" },
+    { name: "StackOverflow", link: "stackoverflow.com" },
+    { name: "FontAwesome", link: "fontawesome.com" },
+];
 
 let searchInput = document.querySelector('#searchInput');
 
@@ -11,15 +28,19 @@ function getItems(e) {
     let searchValue = e.target.value;
     let optionsContainer = document.querySelector('#itemOptions');
 
-    let items = arrayOfObjects.filter(x => x.name.toLowerCase().includes(searchValue.toLowerCase()));
+    if (searchValue != '') {
 
-
-    if (items) {
+        let items = arrayOfObjects.filter(x => x.name.toLowerCase().includes(searchValue.toLowerCase()));
+        if (items) {
+            optionsContainer.innerHTML = "";
+            items.forEach(item => {
+                let newOption = document.createElement('li');
+                newOption.innerHTML = `<a href='https://www.${item.link}' target="_blank">${item.name}</a>`;
+                optionsContainer.append(newOption);
+            })
+        }
+    }
+    else {
         optionsContainer.innerHTML = "";
-        items.forEach(item => {
-            let newOption = document.createElement('li');
-            newOption.innerHTML = `<a href='https://www.${item.link}' target="_blank">${item.name}</a>`;
-            optionsContainer.append(newOption);
-        })
     }
 }
